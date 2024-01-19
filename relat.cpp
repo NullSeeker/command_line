890b91774350974f63406933e3e82446c242bbf4
ChilliWod <yaroslavsurkov2010@gmail.com>
2024-01-19 21:34:22 +0400

    add register(new version)

--- relat.cpp
+++ relat.cpp
@@ -6,13 +6,14 @@
 using namespace std;
 
 class CRegister {
-    
+
 public:
     bool m_Register;
     void Register();
-    
+    string m_Accounts;
+
 private:
-    int m_Accounts;
+      string m_Password;
 };
 
 CRegister m_AccountData;
@@ -21,7 +22,7 @@ CRegister m_AccountData;
 // главная функция(можно и без неё)
 int main()
 {
-    
+
     // переменные
     string aReg;
     string aBuf;
@@ -34,15 +35,15 @@ int main()
 
     // вывод перед циклом
     cout << "Если у вас нету аккаунта то введите команду 'register'\n";
-    
+
     while (keep_pw)
     {
         cout << "Введите пароль от аккаунта: ";
         cin >> aPw;
-        
+
         // пароль
-        
-        
+
+
         if(aPw == "test")
         {
             keep_pw = false;
@@ -55,16 +56,19 @@ int main()
             cout << "Пароль не верный попробуйте снова\n";
             keep_going = false;
             keep_pw = true;
-  
+
         }
 
-        if (m_AccountData.m_Register) {
-            keep_pw = false;
-            keep_going = true;
-            cout << "Вход..\n";
-            cout << "По поводу своих паролей писать в дискорд\n";
+        if(aPw ==m_AccountData.m_Accounts)
+        {
+          keep_pw = false;
+          keep_going = true;
+          cout << "Вход..\n";
+          cout << "По поводу своих паролей писать в дискорд\n";
         }
-        
+
+
+
         if(aPw == "tust")
         {
             keep_pw = false;
@@ -73,42 +77,45 @@ int main()
             cout << "По поводу своих паролей писать в дискорд\n";
         }
 
-        
-        
+
+
         if(aPw == "exit")
         {
             keep_pw = false;
         }
-        
-        if (aPw == "register") {
-        
+
+        if (aPw == "reg") {
+
             m_AccountData.m_Register = true;
             keep_pw = false;
         }
-    
+
         while (m_AccountData.m_Register) {
-            
+
+            cout << "Добро пожаловать в регестрацию\n";
             cout << "Введите новый пароль: ";
             cin >> aReg;
-            
+
+            m_AccountData.m_Accounts = aReg;
+
             cout << "Спасибо за регестрацию, нажмите 'н' что бы выйти\n";
-            
-            if (aReg == "н"){
-                m_AccountData.m_Register = false;
-                keep_pw = true;
-                
-            }
-            
+
+            keep_pw = true;
+            m_AccountData.m_Register = false;
+            break;
+
         }
-    
+
     }
-    
+
+
+
     // цикл
     while (keep_going)
     {
         /* cout вывод сообщения
         cin ввод переменной(делает переменную запоминающей)*/
-        
+
         cout << "Введите сообщение: ";
         cin >> aBuf;
 
@@ -116,28 +123,23 @@ int main()
         if(aBuf == "help")
         {
             // \n перенёс на другую строку
-            cout << "Команды: exit, discord, register( in lo.\n";
+            cout << "Команды: exit, discord, reg( in lo.\n";
         }
-        
-        if(aBuf == "выход")
+
+        if(aBuf == "exit")
         {
             keep_going = false;
         }
-    
-        if(aBuf != "выход")
+
+        if(aBuf == "reg")
         {
-            cout << "Сообщение отправлено!\n";
-        }
-    
-        if(aBuf == "регистрация")
-        {
-            cout << "Для регистрации напишите дискорд\n";
+            cout << "Регистрация: при входе нужно ввести слово \n";
         }
 
-        if(aBuf == "дискорд")
+        if(aBuf == "discrod")
         {
-            cout << "ChillWood#5180\n";
-        }    
-    
+            cout << "saintphnx.";
+        }
+
     }
 }
